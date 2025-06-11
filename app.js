dfd6b26abb2f1a0e0386931e1e31bd61f9e09732
icerockk <isroqaq@gmail.com>
2025-06-12 00:57:26 +0700

    Update app.js
    change directory to match your photo directory

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = 3000;
 
 // Network path to the CaptureResult folder
-const NETWORK_PATH = '\\\\MUNASPRASEJARAH\\CaptureResult'; // Double backslashes for UNC paths
-/* this path could be edited depend on the location of your galery */
+const NETWORK_PATH = path.join('C:', 'Put', 'your', 'directory', 'here'); // Double backslashes for UNC paths
+/* change directory depend on the location of your galery */
 
 // Serve images directly from the network path
 app.use('/images', express.static(NETWORK_PATH));
